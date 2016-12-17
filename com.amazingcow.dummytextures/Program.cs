d196d52062de988f022654926abb7c9f99523b52
n2omatt <n2o.matt@gmail.com>
2016-12-17 21:24:53 -0200

    - First implementation...

---
+++ com.amazingcow.dummytextures/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace com.amazingcow.dummytextures
+{
+    class MainClass
+    {
+        public static void Main(String[] args)
+        {
+            var dir = Environment.GetFolderPath(
+                Environment.SpecialFolder.DesktopDirectory
+            );
+
+            Random rnd = new Random();
+            int id = 0;
+            for(int i = 6; i < 10; ++i)
+            {
+                var size  = (int)(Math.Pow(2, i));
+                var count = rnd.Next(1, 5);
+
+                for(int c = 0; c < count; ++c)
+                {
+                    var color = System.Drawing.Color.FromArgb(
+                        rnd.Next(0, 255),
+                        rnd.Next(0, 255),
+                        rnd.Next(0, 255)
+                    );
+
+                    var texture = new Texture(size, size, color, id);
+                    texture.SaveToFile(dir);
+
+                    id++;
+                }
+            }
+        }
+    
+    }//class MainClass
+
+} // namespace com.amazingcow.dummytextures
+
---
+++ com.amazingcow.dummytextures/Texture.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+
+namespace com.amazingcow.dummytextures
+{
+    public class Texture 
+    {
+        // iVars //
+        private int   m_width;
+        private int   m_height;
+        private Color m_color;
+        private int   m_id;
+
+        // CTOR // 
+        public Texture(int width, int height, Color color, int id)
+        {            
+            m_width  = width;
+            m_height = height;
+            m_color  = color;
+            m_id     = id;
+        }
+
+
+        // Public Methods //
+        public void SaveToFile(String folderPath)
+        {
+            var fullpath = System.IO.Path.Combine(
+                folderPath,
+                m_id.ToString()
+            );
+            fullpath = System.IO.Path.ChangeExtension(fullpath, "png");
+
+            using(Bitmap bitmap = new Bitmap(m_width, m_height))
+            {
+                using(Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using(SolidBrush brush = new SolidBrush(m_color))
+                    {
+                        var rect = new Rectangle(0, 0, m_width, m_height);
+                        graphics.FillRectangle(brush, rect);
+                    }
+                }
+
+                bitmap.Save(
+                    fullpath, 
+                    System.Drawing.Imaging.ImageFormat.Png
+                );
+            }
+        }
+
+    }// class Texture
+
+}// namespace com.amazingcow.dummytextures
+
